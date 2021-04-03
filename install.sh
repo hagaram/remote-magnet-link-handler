f1f122c8d49e727db26e96789daf47ea7483a94b
Michal Hagara <of.hagara@gmail.com>
2021-04-03 13:22:43 +0200

    rewrote printf according to shellcheck guidelines

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ macos_check_reqs () {
   fi
   command -v duti &>/dev/null
   if [ $? -eq 1 ]; then
-    printf "\n${BOLD}Installing duti${NC}\n" ${BOLD} ${NC}
+    printf "\n%bInstalling duti%b\n" "${BOLD}" "${NC}"
     brew install duti
   fi
 }
@@ -29,7 +29,7 @@ linux_check_reqs() {
   command -v curl &>/dev/null
   if [ $? -eq 1 ]; then
     distro=$(sed -nr  "s/^ID=\"?([a-zA-Z]+)\"?/\1/p" /etc/os-release)
-    printf "\n${BOLD}Installing curl${NC}\n"
+    printf "\n%bInstalling curl%b\n" "${BOLD}" "${NC}"
     case $distro in
       arch)
         sudo pacman -Sy curl
@@ -51,8 +51,8 @@ linux_check_reqs() {
 
 
 create_linux_middleware_app () {
-  printf "${BOLD}Creating ${app_name}.desktop entry${NC}\n"
-  mkdir -p ${HOME}/.local/share/applications/
+  printf "%bCreating %s.desktop entry%b\n" "${BOLD}" "${app_name}" "${NC}"
+  mkdir -p "${HOME}/.local/share/applications/"
   cat <<EOF > ${HOME}/.local/share/applications/${app_name}.desktop
 [Desktop Entry]
 Name=${app_name}
@@ -66,59 +66,59 @@ EOF
 
 
 configure_linux_magnet_association () {
-  printf "${BOLD}Assigning association with magnet links${NC}\n"
-  xdg-mime default ${app_name}.desktop x-scheme-handler/magnet
+  printf "%bAssigning association with magnet links%b\n" "${BOLD}" "${NC}"
+  xdg-mime default "${app_name}".desktop x-scheme-handler/magnet
 }
 
 
 create_macos_middleware_app () {
-  printf "${BOLD}Creating ${app_name}.app${NC}"
+  printf "%bCreating %s.app%b" "${BOLD}" "${app_name}" "${NC}"
   cat <<EOF > ${install_path}/${app_name}.scpt
 on open location this_URL
 	do shell script "${install_path}/adder.sh '" & this_URL & "'"
 end open location
 EOF
 
-  osacompile -o /Applications/${app_name}.app ${install_path}/${app_name}.scpt
+  osacompile -o /Applications/"${app_name}".app "${install_path}/${app_name}".scpt
   perl -i -pe 's/(^\s+<key>LSMinimumSystemVersionByArchitecture<\/key>)/\t<key>CFBundleIdentifier<\/key>\n\t<string>com.apple.ScriptEditor.id.Remote-magnet-handler<\/string>\n$1/'  /Applications/${app_name}.app/Contents/Info.plist
 }
 
 
 configure_macos_magnet_association () {
-  printf "\n${BOLD}Assigning association with magnet links${NC}"
+  printf "\n%bAssigning association with magnet links%b" "${BOLD}" "${NC}"
   duti -s com.apple.ScriptEditor.id.Remote-magnet-handler magnet
 }
 
 configure_torrent_client () {
 
   export PS3="Choose number: "
-  printf "${BOLD}Pick torrent client you want to use${NC}:\n"
+  printf "%bPick torrent client you want to use%b:\n" "${BOLD}" "${NC}"
   select client in ${COMPATIBLE_CLIENTS}
   do
     break
   done
 
 
-  printf "\n\n${BOLD}Specify application name you wish to use, leave empty to use default ${GREEN}(defaults to ${DEFAULT_APP_NAME})${NC}:\n"
+  printf "\n\n%bSpecify application name you wish to use, leave empty to use default %b(defaults to %s)%b:\n" "${BOLD}" "${GREEN}" "${DEFAULT_APP_NAME}" "${NC}"
   read app_name
   app_name=${app_name:-"${DEFAULT_APP_NAME}"}
 
-  printf "\n\n${BOLD}Specify installation path where you want scripts to be created, leave empty to use default ${GREEN}(defaults to ${HOME}/software/${app_name})${NC}:\n"
+  printf "\n\n%bSpecify installation path where you want scripts to be created, leave empty to use default %b(defaults to %s/software/%s)%b:\n"  "${BOLD}" "${GREEN}" "${HOME}" "${app_name}" "${NC}"
   read install_path
-  install_path=${install_path:-"$HOME/software/$app_name"}
+  install_path=${install_path:-"${HOME}/software/${app_name}"}
 
-  printf "\n${BOLD}Specify URL of your remote qBittorrent instance ${GREEN}(example: http(s)://192.168.1.200:8080)${NC}:\n"
+  printf "\n%bSpecify URL of your remote qBittorrent instance %b(example: http(s)://192.168.1.200:8080)%b:\n" "${BOLD}" "${GREEN}" "${NC}"
   read url
 
-  printf "\n${BOLD}Specify username  (if applicable) and password, leave both empty when using '${GREEN}Bypass from whitelisted IPs/Auth disabled${NC}' ${BOLD}options${NC}:\n"
-  printf "${BOLD}Input username:${NC}\n"
+  printf "\n%bSpecify username  (if applicable) and password, leave both empty when using '%bBypass from whitelisted IPs/Auth disabled%b' %boptions%b:\n" "${BOLD}" "${GREEN}" "${NC}" "${BOLD}" "${NC}"
+  printf "%bInput username:%b\n" "${BOLD}" "${NC}"
   read username
-  printf "${BOLD}Input password:${NC}\n"
+  printf "%bInput password:%b\n" "${BOLD}" "${NC}"
   read -s password
 
-  printf "\n${BOLD}Creating adder script in ${install_path}/adder.sh${NC}\n"
+  printf "\n%bCreating adder script in %s/adder.sh%b\n" "${BOLD}" "${install_path}" "${NC}"
 
-  mkdir -p ${install_path}
+  mkdir -p "${install_path}"
   cat <<EOF  > ${install_path}/adder.sh
 #!$(which bash)
 
@@ -129,8 +129,8 @@ PASSWORD=$password
 EOF
 
 
-  source templates/${client}
-  chmod +x ${install_path}/adder.sh
+  source templates/"${client}"
+  chmod +x "${install_path}"/adder.sh
 
 
 }
@@ -160,7 +160,7 @@ main () {
       ;;
   esac
   
-  printf "\n${GREEN}DONE${NC}\n"
+  printf "\n%bDONE%b\n" "${GREEN}" "${NC}"
 }
 
 main
